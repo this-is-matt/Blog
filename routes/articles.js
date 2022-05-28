bb8f5b26a34772c804a49fae0885e9b4c6a8d993
this-is-matt <mcp6000@gmail.com>
2022-05-28 12:07:52 -0600

    Added a PUT transaction. Doesn't include much validation yet

--- routes/articles.js
+++ routes/articles.js
@@ -3,30 +3,57 @@ const router = express.Router();
 const Article = require('../Article');
 
 router
-.get('/', async (req,res) => {
-    // res.send('We are on articles');
-    try{
-        const articles =  await Article.find();
-        res.json(articles);
-    } catch (err){
-        res.json({message:err});  
-    }
-})
-.post('/', async (req, res) => {
-    const article = new Article({
-        title: req.body.title,
-        copy: req.body.copy
-    });
-
-    try{
-        const savedArticle = await article.save();
-        res.json(savedArticle);
-    } catch (err) {
-        res.json({
-            message: err
+    .get('/', async (req, res) => {
+        // res.send('We are on articles');
+        try {
+            const articles = await Article.find();
+            res.json(articles);
+        } catch (err) {
+            res.json({
+                message: err
+            });
+        }
+    })
+    .post('/', async (req, res) => {
+        const article = new Article({
+            title: req.body.title,
+            copy: req.body.copy
         });
-    }
-});
+
+        try {
+            const savedArticle = await article.save();
+            res.json(savedArticle);
+        } catch (err) {
+            res.json({
+                message: err
+            });
+        }
+    })
+    .put('/:id', (req, res) => {
+        const id = req.params.id;
+        Article.findByIdAndUpdate(id, 
+            {
+                title: req.body.title,
+                copy: req.body.copy
+            }, 
+            {
+                useFindAndModify: false
+            })
+            .then(data => {
+                if (!data) {
+                    res.status(404).send({
+                        message: `Cannot update article with id=${id}.`
+                    });
+                } else res.send({
+                    message: "Article was updated successfully."
+                });
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: "Error Updating Article with id=" + id
+                });
+            });
+    })
 
 
 module.exports = router
